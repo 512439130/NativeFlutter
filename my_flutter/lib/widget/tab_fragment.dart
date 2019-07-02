9b2b5a71208fd01b72830ca562d039a58f43db4a
SoMustYY <13160677911@163.com>
2019-07-02 15:00:44 +0800

    新增网络请求库（dio），简单封装请求库，示例编写

--- my_flutter/lib/widget/tab_fragment.dart
+++ my_flutter/lib/widget/tab_fragment.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
+import 'package:dio/dio.dart';
 import "package:flutter/widgets.dart";
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_flutter/util/http_util.dart';
 import 'package:my_flutter/util/navigator_util.dart';
 import 'package:my_flutter/util/toast_util.dart';
 
@@ -13,6 +15,8 @@ class TabFragment extends StatefulWidget {
 }
 
 class _TabFragmentState extends State<TabFragment> {
+  
+  String text = "这是默认内容";
   @override
   void initState() {
     // TODO: implement initState
@@ -40,13 +44,54 @@ class _TabFragmentState extends State<TabFragment> {
                       fontWeight: FontWeight.normal,
                       decoration: TextDecoration.none),
                 ),
-              ),
 
+              ),
+              new Padding(padding: EdgeInsets.only(top: 200)),
+              new Container(
+                width: 100,
+                alignment: Alignment.center,
+                child: new MaterialButton(
+                  child: new Text("网络请求测试"),
+                  color: Colors.greenAccent,
+                  onPressed: () {
+                    buttonClick();
+                  },
+                ),
+              ),
+              new Container(
+                alignment: Alignment.center,
+                child: new Text(
+                  text,
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none),
+                ),
+
+              ),
             ],
           ),
         ),
       ),
     );
+
+  }
+
+  void buttonClick() async{
+    Response response = await HttpUtil().doGet("api/test");
+    if (response != null) {
+      if (response.statusCode == 200) {
+        setState(() {
+          print("请求成功-response:"+response.data.toString());
+          text = response.data.toString();
+        });
+      } else {
+        print("请求失败，请检查网络后重试");
+      }
+    } else {
+      print("请求失败，请检查网络后重试");
+    }
   }
 
 }
